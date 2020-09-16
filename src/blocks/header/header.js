743e6111b18eaa9a0b75921874a45e24607d6d14
Andreikass <starforg-revolution@yandex.ru>
2020-09-16 16:58:54 +0500

    add psges product-pages

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,55 +1,59 @@
 const header = () => {
-  $('.header__btn').click(function() {
-    if($('body .header__btn--opened').length) {
-      $('.header__right').fadeOut(100);
-    } else {
-      $('.header__right').fadeIn(100);
-    }
+	$(".header__btn").click(function () {
+		if ($("body .header__btn--opened").length) {
+			$(".header__right").fadeOut(100);
+		} else {
+			$(".header__right").fadeIn(100);
+		}
 
-    $('.header__right').toggleClass('header__mobile');
-    $('.header__btn').toggleClass('header__btn--opened');
-    $('body').toggleClass('overflow-heidden ');
-  });
+		$(".header__right").toggleClass("header__mobile");
+		$(".header__btn").toggleClass("header__btn--opened");
+		$("body").toggleClass("overflow-heidden ");
+	});
+	$(window).resize(function () {
+		if ($(window).width() > 991) {
+			$(".header__btn").removeClass("header__btn--opened");
+		}
+	});
+	let scrolled = 0;
 
-  let scrolled = 0;
+	changeScrolled();
 
-  changeScrolled();
+	$(window).scroll(function () {
+		let $this = $(this),
+			st = $this.scrollTop();
 
-  $(window).scroll(function(){
-    let $this = $(this),
-        st = $this.scrollTop();
+		menuScrollDesktop(st);
+	});
 
-    menuScrollDesktop(st);
-  });
+	$(window).resize(function () {
+		let st = $(window).width();
 
-  $(window).resize(function(){
-    let st = $(window).width();
+		menuDesktopShow(st);
+		changeScrolled();
+	});
 
-    menuDesktopShow(st);
-    changeScrolled();
-  });
+	function menuScrollDesktop(st) {
+		if (st > scrolled) {
+			$("body").addClass("scrolled");
+		} else {
+			$("body").removeClass("scrolled");
+		}
+	}
 
-  function menuScrollDesktop(st) {
-    if(st > scrolled){
-      $('body').addClass('scrolled');
-    }else{
-      $('body').removeClass('scrolled');
-    }
-  };
+	function menuDesktopShow(st) {
+		if (st > 992) {
+			$(".header__right").attr("style", "");
 
-  function menuDesktopShow(st) {
-    if(st > 992) {
-      $('.header__right').attr('style', '');
-      
-      if($('.overflow-heidden').length) {
-        $('body').removeClass('overflow-heidden');
-      }
-    }
-  };
+			if ($(".overflow-heidden").length) {
+				$("body").removeClass("overflow-heidden");
+			}
+		}
+	}
 
-  function changeScrolled() {
-      scrolled = 0;
-  };
-}
+	function changeScrolled() {
+		scrolled = 0;
+	}
+};
 
 export default header;
--- src/blocks/shop/shop.js
+++ src/blocks/shop/shop.js
@@ -1,28 +1,28 @@
-import city from '../city/city';
-import shopItem from '../shop-item/shop-item';
-import cart from '../shop-sidebar/shop-sidebar';
+import city from "../city/city";
+import shopItem from "../shop-item/shop-item";
+import cart from "../shop-sidebar/shop-sidebar";
 
 const shop = () => {
-  loadShopCatTitles();
-  city();
-  shopItem();
-  cart();
-}
+	loadShopCatTitles();
+	city();
+	shopItem();
+	cart();
+};
 
 function loadShopCatTitles() {
-  let catTitles = {
-    borshch_s_mitbolami: 'Борщ',
-    solyanka_s_mitbolami: 'Солянка',
-    suplapsha_s_mitbolami: 'Суп лапша куриная',
-    lagman_s_mitbolami: 'Лагман',
-    tom_yam_s_fishbolami: 'Том Ям',
-    tom_kkha_s_mitbolami: 'Том Кха',
-    ma_po_tofu_s_farshem: 'Ма По Тофу',
-    fo_s_mitbolami: 'Фо с митболами',
-    miso_s_fishbolami: 'Мисо'
-  }
+	let catTitles = {
+		borshch_s_mitbolami: "Борщ",
+		solyanka_s_mitbolami: "Солянка",
+		suplapsha_s_mitbolami: "Суп лапша куриная",
+		lagman_s_mitbolami: "Лагман",
+		tom_yam_s_fishbolami: "Том Ям",
+		tom_kkha_s_mitbolami: "Том Кха",
+		ma_po_tofu_s_farshem: "Ма По Тофу",
+		fo_s_mitbolami: "Фо с митболами",
+		miso_s_fishbolami: "Мисо",
+	};
 
-  localStorage.setItem('catTitles', JSON.stringify(catTitles));
+	localStorage.setItem("catTitles", JSON.stringify(catTitles));
 }
 
 export default shop;
